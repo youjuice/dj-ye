7b9f16a9050c94f4d00ff5bb12b0e0c22eec272e
yooju00 <yooju00@naver.com>
2024-09-04 03:27:17 +0900

    Refactor: playlist_manager 모듈화

---
+++ utils/playlist_manager.py
@@ -0,0 +1,33 @@
+class PlaylistManager:
+    def __init__(self):
+        self.playlist = []
+        self.current_index = -1
+
+    def add_song(self, song):
+        self.playlist.append(song)
+        if self.current_index == -1:
+            self.current_index = 0
+
+    def get_next_song(self):
+        if not self.playlist:
+            return None
+        self.current_index = (self.current_index + 1) % len(self.playlist)
+        return self.playlist[self.current_index]
+
+    def get_previous_song(self):
+        if not self.playlist:
+            return None
+        self.current_index = (self.current_index - 1) % len(self.playlist)
+        return self.playlist[self.current_index]
+
+    def get_current_song(self):
+        if not self.playlist or self.current_index == -1:
+            return None
+        return self.playlist[self.current_index]
+
+    def clear_playlist(self):
+        self.playlist = []
+        self.current_index = -1
+
+    def get_playlist(self):
+        return self.playlist

aa02e25489a95b7f2e0554d0b7b1fde4db9c5d9f
yooju00 <yooju00@naver.com>
2024-09-04 01:27:34 +0900

    Feat: pause & resume 명령어 추가

--- cogs/music_player.py
+++ cogs/music_player.py
@@ -1,69 +1,10 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-import asyncio
-import yt_dlp as youtube_dl
-
-# Suppress noise about console usage from errors
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0'
-}
-
-ffmpeg_options = {
-    'options': '-vn -b:a 128k',
-    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+from utils.ytdlsource import YTDLSource
 
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title = data.get('title')
-        self.url = ""
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        try:
-            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-            if 'entries' in data:
-                # 재생 목록에서 첫 번째 항목 가져오기
-                data = data['entries'][0]
-            filename = data['url'] if stream else ytdl.prepare_filename(data)
-            return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-        except Exception as e:
-            print(f"An error occurred while processing the URL: {e}")
-            raise
-
-    @classmethod
-    async def search_source(cls, search: str, *, loop: asyncio.BaseEventLoop = None, download=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(f"ytsearch:{search}", download=download))
-
-        if 'entries' not in data:
-            raise ValueError("Couldn't find any video matching the search query.")
-
-        data = data['entries'][0]
-
-        filename = data['url'] if not download else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-
-class Music(commands.Cog):
+class MusicPlayer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -74,8 +15,6 @@ class Music(commands.Cog):
             else:
                 await interaction.followup.send("음성 채널에 먼저 참가해주세요!")
                 raise commands.CommandError("Author not connected to a voice channel.")
-        elif interaction.guild.voice_client.is_playing():
-            interaction.guild.voice_client.stop()
 
     @app_commands.command(name="play", description="Enter Song Title & Artist")
     @app_commands.describe(title="Title", artist="Artist")
@@ -99,15 +38,16 @@ class Music(commands.Cog):
     @app_commands.describe(url="YouTube URL")
     async def play_url(self, interaction: discord.Interaction, url: str):
         await interaction.response.defer()
-
         try:
             await self.ensure_voice(interaction)
             async with interaction.channel.typing():
                 source = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
 
-            interaction.guild.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
-
-            await interaction.followup.send(f'🎧 Now playing: {source.title}')
+            if interaction.guild.voice_client.is_playing():
+                await interaction.followup.send("노래가 이미 재생 중입니다. 플레이리스트에 추가하려면 /add_playlist 명령어를 사용해주세요.")
+            else:
+                interaction.guild.voice_client.play(source, after=lambda e: print(f'Player error: {e}') if e else None)
+                await interaction.followup.send(f'🎧 Now playing: {source.title}')
         except Exception as e:
             await interaction.followup.send(f"An error occurred: {str(e)}")
             print(f"Detailed error: {e}")
@@ -126,6 +66,25 @@ class Music(commands.Cog):
         await interaction.guild.voice_client.disconnect()
         await interaction.response.send_message("⏹️ Disconnected from voice channel.")
 
+    @app_commands.command(name="pause", description="Pause the current music")
+    async def pause(self, interaction: discord.Interaction):
+        if interaction.guild.voice_client and interaction.guild.voice_client.is_playing():
+            interaction.guild.voice_client.pause()
+            await interaction.response.send_message("⏹️ The music has been paused.")
+        else:
+            await interaction.response.send_message("현재 재생 중인 음악이 없습니다.")
+
+    @app_commands.command(name="resume", description="Resume the paused music")
+    async def resume(self, interaction: discord.Interaction):
+        if interaction.guild.voice_client and interaction.guild.voice_client.is_paused():
+            interaction.guild.voice_client.resume()
+            await interaction.response.send_message("The music has been resumed.")
+        elif interaction.guild.voice_client and interaction.guild.voice_client.is_playing():
+            await interaction.response.send_message("The music is already playing.")
+        else:
+            await interaction.response.send_message(
+                "There is no music to resume. Use the play command to start playing music.")
+
 
 async def setup(bot):
-    await bot.add_cog(Music(bot))
+    await bot.add_cog(MusicPlayer(bot))

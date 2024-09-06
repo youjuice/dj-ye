7b4fa4c50e3fdf197a898fa0043b602a9f873302
yooju00 <yooju00@naver.com>
2024-09-07 00:17:29 +0900

    Feat: shuffle, jump, remove 명령어 추가

--- cogs/playlist_controller.py
+++ cogs/playlist_controller.py
@@ -27,3 +27,42 @@ class PlaylistController:
         else:
             playlist_text = "\n".join([f"{i + 1}. {song['title']} - {song['artist']}" for i, song in enumerate(playlist)])
             await interaction.response.send_message(f"♡ Your playlist:\n{playlist_text}")
+
+    @app_commands.command(name="remove", description="Remove a song from the playlist")
+    @app_commands.describe(index="The index of the song to remove")
+    async def remove_song(self, interaction: discord.Interaction, index: int):
+        guild_id = interaction.guild_id
+        playlist_manager = self.get_playlist_manager(guild_id)
+        removed_song = playlist_manager.remove_song(index - 1)
+        if removed_song:
+            await interaction.response.send_message(f"Removed song: {removed_song['title']} - {removed_song['artist']}")
+        else:
+            await interaction.response.send_message("Invalid index. No song removed.")
+
+    @app_commands.command(name="jump", description="Jump to a specific song in the playlist and play it")
+    @app_commands.describe(index="The index of the song to jump to")
+    async def jump_to_song(self, interaction: discord.Interaction, index: int):
+        guild_id = interaction.guild_id
+        playlist_manager = self.get_playlist_manager(guild_id)
+        song = playlist_manager.jump_to_song(index - 1)
+        if song:
+            await interaction.response.send_message(f"Jumped to song: {song['title']} - {song['artist']}")
+            voice_client = interaction.guild.voice_client
+            if voice_client:
+                # 현재 재생 중인 노래 중지
+                if voice_client.is_playing():
+                    voice_client.stop()
+
+                # play_song 메서드를 호출하여 새로운 노래 재생
+                await self.play_song(voice_client, guild_id)
+            else:
+                await interaction.followup.send("Bot is not connected to a voice channel. Use the play command first.")
+        else:
+            await interaction.response.send_message("Invalid index. Couldn't jump to the song.")
+
+    @app_commands.command(name="shuffle", description="Shuffle the playlist")
+    async def shuffle_playlist(self, interaction: discord.Interaction):
+        guild_id = interaction.guild_id
+        playlist_manager = self.get_playlist_manager(guild_id)
+        playlist_manager.shuffle_playlist()
+        await interaction.response.send_message("Playlist has been shuffled!")
--- utils/playlist_manager.py
+++ utils/playlist_manager.py
@@ -1,3 +1,6 @@
+import random
+
+
 class PlaylistManager:
     def __init__(self):
         self.playlist = []
@@ -29,3 +32,27 @@ class PlaylistManager:
 
     def get_playlist(self):
         return self.playlist
+
+    def remove_song(self, index):
+        if 0 <= index < len(self.playlist):
+            removed_song = self.playlist.pop(index)
+            if index < self.current_index:
+                self.current_index -= 1
+            elif index == self.current_index:
+                self.current_index = min(self.current_index, len(self.playlist) - 1)
+            return removed_song
+        return None
+
+    def jump_to_song(self, index):
+        if 0 <= index < len(self.playlist):
+            self.current_index = index
+            return self.playlist[self.current_index]
+        return None
+
+    def shuffle_playlist(self):
+        current_song = self.playlist[self.current_index] if self.current_index != -1 else None
+        random.shuffle(self.playlist)
+        if current_song:
+            self.current_index = self.playlist.index(current_song)
+        else:
+            self.current_index = -1

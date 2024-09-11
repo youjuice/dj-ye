addd0490dd47bdac4f2b38227c7fa623d2c65281
yooju00 <yooju00@naver.com>
2024-09-11 21:20:57 +0900

    Modify: yt-dlp 오류 수정

--- utils/ytdl_source.py
+++ utils/ytdl_source.py
@@ -24,9 +24,7 @@ ytdl_format_options = {
     'source_address': '0.0.0.0',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
     'prefer_ffmpeg': True,
-    'cachedir': False,
-    'geo_bypass': True,
-    'extractor_retries': '3',
+    'cachedir': False
 }
 
 ffmpeg_options = {
